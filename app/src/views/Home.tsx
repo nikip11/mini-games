cb2facbcf5e44e67488faee70c6fe3de29957875
Niki Joel <niki_ponce@hotmail.com>
2022-04-02 22:33:18 +0200

    link

--- app/src/views/Home.tsx
+++ app/src/views/Home.tsx
@@ -1,31 +1,14 @@
 import SimpleContainer from '@/components/ui/SimpleContainer'
-import useFilter from '@/hooks/useFilter'
-import { words } from '@/mocks'
 import { Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
-  const data = useFilter(
-    words,
-    [
-      {
-        key: 'name',
-        value: 'perro'
-      },
-      {
-        key: 'name',
-        value: 'gato'
-      }
-    ],
-    'OR'
-  )
-
-  console.log({ data })
-
   return (
     <SimpleContainer>
       <Typography variant="h2" sx={{ textAlign: 'center' }}>
         Mini Juegos
       </Typography>
+      <Link to="/words">Words</Link>
     </SimpleContainer>
   )
 }
